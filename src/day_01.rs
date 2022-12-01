d7fc9390ac15b15998e1cddd105ead39fb2b9587
Viktor Lazarev <taurus101v@gmail.com>
2022-12-01 20:33:02 +0100

    Solve day 1, part 2

--- src/day_01.rs
+++ src/day_01.rs
@@ -5,6 +5,13 @@ pub fn part_1() -> u32 {
     split_by_elfs(&lines).into_iter().max().unwrap()
 }
 
+pub fn part_2() -> u32 {
+    let mut elfs = split_by_elfs(&read_lines(1));
+    elfs.sort();
+    elfs.reverse();
+    elfs.into_iter().take(3).sum()
+}
+
 fn split_by_elfs(lines: &Vec<String>) -> Vec<u32> {
     let mut elfs = vec![];
 
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod utils;
 
 fn main() {
     println!("01 / 01: {}", day_01::part_1());
+    println!("01 / 02: {}", day_01::part_2());
 }
