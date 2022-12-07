2add32422f7e36d1ed773b6c913276ab3fe4c426
Viktor Lazarerv <taurus101v@gmail.com>
2022-12-07 19:59:52 +0100

    Fix typo

--- src/day_06.rs
+++ src/day_06.rs
@@ -12,7 +12,7 @@ pub fn part_2() -> usize {
 
 fn find_start(line: &str, uniques: usize) -> usize {
     for index in 0..line.len() - uniques {
-        if !contains_dublicates(&line[index..index + uniques]) {
+        if !contains_duplicates(&line[index..index + uniques]) {
             return index + uniques;
         }
     }
@@ -20,7 +20,7 @@ fn find_start(line: &str, uniques: usize) -> usize {
     0
 }
 
-fn contains_dublicates(line: &str) -> bool {
+fn contains_duplicates(line: &str) -> bool {
     line.char_indices()
         .find(|(index, char)| line[index + 1..].find(*char).is_some())
         .is_some()
