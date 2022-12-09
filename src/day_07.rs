22bb02443548918a3d0e859b79cda024e2de6029
Viktor Lazarev <taurus101v@gmail.com>
2022-12-09 17:00:47 +0100

    Day 7: create fs structure

--- src/day_07.rs
+++ src/day_07.rs
@@ -1,11 +1,17 @@
+use std::collections::HashMap;
+
 use regex::Regex;
 
 use crate::utils::read_lines;
 
 pub fn part_1() -> u32 {
+    let mut fs = Fs::new();
+
     for line in read_lines(7).iter().map(parse_line) {
-        println!("{:?}", line);
+        fs.process_line(line);
     }
+
+    println!("FS: {:?}", fs);
 
     0
 }
@@ -24,7 +30,7 @@ enum Command {
 
 #[derive(Debug)]
 enum OutputLine {
-    File(String, u32),
+    File(File),
     Dir(String),
 }
 
@@ -48,14 +54,113 @@ fn parse_command(line: &str) -> Command {
 }
 
 fn parse_output(line: &str) -> OutputLine {
-    use OutputLine::*;
-
     if line.starts_with("dir ") {
-        return Dir(line[4..].to_owned());
+        return OutputLine::Dir(line[4..].to_owned());
     }
 
     let template = Regex::new(r"^(\d+) (.+)$").unwrap();
     let captures = template.captures(line).unwrap();
 
-    File(captures[2].to_owned(), captures[1].parse::<u32>().unwrap())
+    OutputLine::File(File {
+        name: captures[2].to_owned(),
+        size: captures[1].parse::<u32>().unwrap(),
+    })
+}
+
+#[derive(Debug)]
+struct Fs {
+    dirs: Vec<Dir>,
+    indexed_path: Vec<usize>,
+}
+
+impl Fs {
+    pub fn new() -> Self {
+        Self {
+            dirs: vec![Dir::root()],
+            indexed_path: vec![0],
+        }
+    }
+
+    pub fn process_line(&mut self, line: Line) {
+        match line {
+            Line::Cmd(cmd) => self.process_cmd(cmd),
+            Line::Output(output) => self.process_output(output),
+        }
+    }
+
+    fn process_cmd(&mut self, cmd: Command) {
+        match cmd {
+            Command::Cd(path) => self.process_cd_cmd(&path),
+            Command::Ls => return,
+        }
+    }
+
+    fn process_cd_cmd(&mut self, path: &str) {
+        match path {
+            "/" => self.indexed_path = vec![0],
+            ".." => {
+                if self.indexed_path.len() > 1 {
+                    self.indexed_path.pop();
+                }
+            }
+            _ => {
+                let current_dir_index = self.indexed_path.last().unwrap();
+
+                match self.dirs[*current_dir_index].dirs.get(path) {
+                    None => println!("There's no such a nested dir: {path}"),
+                    Some(dir_index) => {
+                        self.indexed_path.push(*dir_index);
+                    }
+                }
+            }
+        }
+    }
+
+    fn process_output(&mut self, output: OutputLine) {
+        match output {
+            OutputLine::Dir(dir_name) => {
+                let current_dir_index = self.indexed_path.last().unwrap();
+
+                self.dirs.push(Dir {
+                    name: dir_name.to_owned(),
+                    parent_dir_index: Some(*current_dir_index),
+                    ..Default::default()
+                });
+
+                let dir_index = self.dirs.len() - 1;
+
+                self.dirs[*current_dir_index]
+                    .dirs
+                    .insert(dir_name.to_owned(), dir_index);
+            }
+            OutputLine::File(file) => {
+                let current_dir_index = self.indexed_path.last().unwrap();
+
+                self.dirs[*current_dir_index].files.push(file);
+            }
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct Dir {
+    name: String,
+    parent_dir_index: Option<usize>,
+    files: Vec<File>,
+    dirs: HashMap<String, usize>,
+}
+
+impl Dir {
+    pub fn root() -> Self {
+        Self {
+            name: "/".to_owned(),
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    size: u32,
 }
