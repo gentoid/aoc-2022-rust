c601fd07bae5f1dc137a2f85063b83eb70644def
Viktor Lazarev <taurus101v@gmail.com>
2022-12-15 17:42:18 +0100

    Collect unique pairs of coordinates

--- src/day_14.rs
+++ src/day_14.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 
 use crate::utils::read_lines;
@@ -11,11 +13,38 @@ pub fn part_1() {
     let min = by_condition(&walls, Coord::max(), Coord::min_of);
     let max = by_condition(&walls, Coord::min(), Coord::max_of);
 
+    let mut pairs = HashSet::new();
+
+    for wall in &walls {
+        for pair in wall.windows(2) {
+            let one = pair[0].clone();
+            let two = pair[1].clone();
+
+            pairs.insert(if one > two { (two, one) } else { (one, two) });
+        }
+    }
+
+    println!("Found {} unique pairs:\n{:#?}", pairs.len(), pairs);
+
+    // let mut coords = walls[0];
+
     println!("Min: {:?}", min);
     println!("Max: {:?}", max);
+
+    for y in 0..=max.y {
+        print!("{y}");
+        for x in min.x..=max.x {
+            if walls[0].contains(&Coord { x, y }) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!("");
+    }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq, PartialOrd)]
 struct Coord {
     x: usize,
     y: usize,
