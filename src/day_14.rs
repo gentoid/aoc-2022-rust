015a71426acdd4443ab46f764598e423556c1fec
Viktor Lazarev <taurus101v@gmail.com>
2022-12-16 23:36:09 +0100

    Day 14: refactor the code

--- src/day_14.rs
+++ src/day_14.rs
@@ -5,8 +5,8 @@ use itertools::Itertools;
 
 use crate::utils::read_lines;
 
-type Wall = Vec<Coord>;
-type Cave = HashMap<Coord, CellType>;
+type WallCorners = Vec<Coord>;
+type Cells = HashMap<Coord, CellType>;
 type Pairs = HashSet<(Coord, Coord)>;
 
 pub fn part_1() -> usize {
@@ -20,55 +20,37 @@ pub fn solve(input: &[String]) -> usize {
         .map(|line| parse_line(line))
         .collect_vec();
 
-    let min = by_condition(&walls, Coord::max(), Coord::min_of);
-    let max = by_condition(&walls, Coord::min(), Coord::max_of);
-
     let pairs = extract_pairs(&walls);
 
     println!("Found {} unique pairs", pairs.len());
 
-    println!("Min: {:?}", min);
-    println!("Max: {:?}", max);
-
     let mut cave: Cave = prepare_cave(pairs);
 
-    let mut particle = start_position();
-    cave.insert(particle.clone(), CellType::Sand);
+    println!("Min: {:?}", cave.min);
+    println!("Max: {:?}", cave.max);
 
-    let mut counter = 0;
-    let mut particles = 0;
-    let floor = max.y + 1;
+    cave.new_particle();
 
     loop {
-        let (updated_cave, next_particle, add_more) = tick(cave, &floor, &particle);
+        cave.tick();
 
-        cave = updated_cave;
-        if !add_more {
+        if cave.fullfilled {
             println!("No more particles can be held");
             break;
         }
 
-        counter += 1;
-
-        if let Some(next_coord) = next_particle {
-            particle = next_coord;
-            continue;
-        } else {
-            particles += 1;
-            particle = start_position();
-            cave.insert(particle.clone(), CellType::Sand);
+        if let None = cave.current_particle {
+            cave.new_particle();
         }
     }
 
-    println!("Counter end: {counter}");
-    println!("Added {particles} particles");
-
-    for y in 0..=max.y {
+    for y in 0..=cave.max.y {
         print!("{y}");
-        for x in min.x..=max.x {
-            if x == 500 && y == 0 {
+        for x in cave.min.x..=cave.max.x {
+            let coord = Coord { x, y };
+            if coord == cave.start_coord {
                 print!("S");
-            } else if let Some(cell_type) = cave.get(&Coord { x, y }) {
+            } else if let Some(cell_type) = cave.cells.get(&coord) {
                 print!("{cell_type}");
             } else {
                 print!(".");
@@ -77,9 +59,13 @@ pub fn solve(input: &[String]) -> usize {
         println!("");
     }
 
-    particles
+    cave.cells
+        .iter()
+        .filter(|cell| *cell.1 == CellType::Sand)
+        .count()
 }
 
+#[derive(PartialEq)]
 enum CellType {
     Wall,
     Sand,
@@ -93,6 +79,107 @@ impl fmt::Display for CellType {
         };
 
         write!(f, "{display_as}")
+    }
+}
+
+enum BottomType {
+    Floor,
+    Void,
+}
+
+struct Bottom {
+    bottom_type: BottomType,
+    add_levels: usize,
+}
+
+struct Cave {
+    cells: Cells,
+    start_coord: Coord,
+    current_particle: Option<Coord>,
+    bottom: Bottom,
+    fullfilled: bool,
+    min: Coord,
+    max: Coord,
+}
+
+impl Cave {
+    fn new(cells: Cells) -> Self {
+        let wall_cells = cells
+            .iter()
+            .filter(|(_, cell_type)| **cell_type == CellType::Wall)
+            .map(|(coord, _)| coord.clone())
+            .collect_vec();
+
+        Self {
+            cells,
+            start_coord: Coord { x: 500, y: 0 },
+            current_particle: None,
+            bottom: Bottom {
+                bottom_type: BottomType::Void,
+                add_levels: 0,
+            },
+            fullfilled: false,
+            min: wall_cells.iter().fold(Coord::max(), Coord::min_of),
+            max: wall_cells.iter().fold(Coord::min(), Coord::max_of),
+        }
+    }
+
+    fn new_particle(&mut self) {
+        if self.cells.contains_key(&self.start_coord) {
+            self.fullfilled = true;
+            return;
+        }
+
+        self.current_particle = Some(self.start_coord.clone());
+        self.cells.insert(self.start_coord.clone(), CellType::Sand);
+    }
+
+    fn tick(&mut self) {
+        let current_particle = self
+            .current_particle
+            .clone()
+            .unwrap_or(self.start_coord.clone());
+
+        let next_coord = current_particle.down();
+        let moved = self.move_particle(&current_particle, next_coord);
+
+        if moved {
+            return;
+        }
+
+        let next_coord = current_particle.down_left();
+        let moved = self.move_particle(&current_particle, next_coord);
+
+        if moved {
+            return;
+        }
+
+        let next_coord = current_particle.down_right();
+        let moved = self.move_particle(&current_particle, next_coord);
+
+        if moved {
+            return;
+        }
+
+        self.current_particle = None;
+    }
+
+    fn move_particle(&mut self, from: &Coord, to: Coord) -> bool {
+        if from.y >= (self.max.y + self.bottom.add_levels) {
+            self.fullfilled = true;
+            self.current_particle = None;
+            self.cells.remove(&from);
+            return true;
+        }
+
+        if !self.cells.contains_key(&to) {
+            let value = self.cells.remove(&from).unwrap();
+            self.cells.insert(to.clone(), value);
+            self.current_particle = Some(to);
+            return true;
+        }
+
+        false
     }
 }
 
@@ -114,14 +201,14 @@ impl Coord {
         }
     }
 
-    fn min_of(one: &Coord, other: &Self) -> Self {
+    fn min_of(one: Self, other: &Self) -> Self {
         Self {
             x: one.x.min(other.x),
             y: one.y.min(other.y),
         }
     }
 
-    fn max_of(one: &Coord, other: &Self) -> Self {
+    fn max_of(one: Self, other: &Self) -> Self {
         Self {
             x: one.x.max(other.x),
             y: one.y.max(other.y),
@@ -165,68 +252,11 @@ fn parse_cord(input: &str) -> Coord {
     }
 }
 
-fn by_condition(
-    coords: &Vec<Vec<Coord>>,
-    init: Coord,
-    select: fn(&Coord, &Coord) -> Coord,
-) -> Coord {
-    coords.iter().fold(init, |result, wall| {
-        let in_wall = wall
-            .iter()
-            .fold(result.clone(), |inner, coord| select(&inner, coord));
-        select(&result, &in_wall)
-    })
-}
-
 fn min_max(one: &usize, two: &usize) -> (usize, usize) {
     (*one.min(two), *one.max(two))
 }
 
-fn tick(cave: Cave, floor: &usize, particle: &Coord) -> (Cave, Option<Coord>, bool) {
-    let next_coord = particle.down();
-
-    if next_coord.y >= *floor {
-        return (cave, None, false);
-    }
-
-    let (cave, moved) = move_particle(cave, particle, &next_coord);
-
-    if moved {
-        return (cave, Some(next_coord), true);
-    }
-
-    let next_coord = particle.down_left();
-    let (cave, moved) = move_particle(cave, particle, &next_coord);
-
-    if moved {
-        return (cave, Some(next_coord), true);
-    }
-
-    let next_coord = particle.down_right();
-    let (cave, moved) = move_particle(cave, particle, &next_coord);
-
-    if moved {
-        return (cave, Some(next_coord), true);
-    }
-
-    (cave, None, true)
-}
-
-fn move_particle(mut cave: Cave, from: &Coord, to: &Coord) -> (Cave, bool) {
-    if !cave.contains_key(&to) {
-        let value = cave.remove(&from).unwrap();
-        cave.insert(to.clone(), value);
-        return (cave, true);
-    }
-
-    (cave, false)
-}
-
-fn start_position() -> Coord {
-    Coord { x: 500, y: 0 }
-}
-
-fn extract_pairs(walls: &[Wall]) -> Pairs {
+fn extract_pairs(walls: &[WallCorners]) -> Pairs {
     let mut pairs = HashSet::new();
 
     for wall in walls {
@@ -242,7 +272,7 @@ fn extract_pairs(walls: &[Wall]) -> Pairs {
 }
 
 fn prepare_cave(pairs: Pairs) -> Cave {
-    let mut cave = HashMap::new();
+    let mut cells = HashMap::new();
 
     for (from, to) in pairs {
         if from.x != to.x {
@@ -250,17 +280,17 @@ fn prepare_cave(pairs: Pairs) -> Cave {
             let y = from.y;
 
             for x in min..=max {
-                cave.insert(Coord { x, y }, CellType::Wall);
+                cells.insert(Coord { x, y }, CellType::Wall);
             }
         } else if from.x != to.y {
             let (min, max) = min_max(&from.y, &to.y);
             let x = from.x;
 
             for y in min..=max {
-                cave.insert(Coord { x, y }, CellType::Wall);
+                cells.insert(Coord { x, y }, CellType::Wall);
             }
         }
     }
 
-    cave
+    Cave::new(cells)
 }
