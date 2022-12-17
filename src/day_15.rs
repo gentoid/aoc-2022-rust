509252b71547abe1e23f97bb9838bba3f29ffc0a
Viktor Lazarev <taurus101v@gmail.com>
2022-12-17 14:52:18 +0100

    Calculate max ranges for a given y line

--- src/day_15.rs
+++ src/day_15.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Debug, ops::Range, os::windows::process};
+
 use itertools::Itertools;
 use regex::Regex;
 
@@ -27,17 +29,45 @@ pub fn part_1() -> usize {
         .filter(|beacon| beacon.y == y)
         .count();
 
-    (min_x..=max_x)
-        .map(|x| {
-            let coord = Coord { x, y };
-            scanners
-                .iter()
-                .find(|scanner| scanner.can_be_scanned(&coord))
-                .is_some()
-        })
-        .filter(|found| *found)
-        .count()
-        - beacons_on_line
+    // (min_x..=max_x)
+    //     .map(|x| {
+    //         let coord = Coord { x, y };
+    //         scanners
+    //             .iter()
+    //             .find(|scanner| scanner.can_be_scanned(&coord))
+    //             .is_some()
+    //     })
+    //     .filter(|found| *found)
+    //     .count()
+    //     - beacons_on_line
+
+    let mut ranges = scanners
+        .iter()
+        .map(|sensor| sensor.range_for_y(&y))
+        .filter(|range| !range.is_empty())
+        .collect_vec();
+
+    println!("Ranges:\n{:?}", ranges);
+
+    // let mut merged_ranges = vec![];
+
+    let mut iterations = 0;
+    loop {
+        let (merged, merged_at_least_once) = merge_ranges(&ranges);
+        ranges = merged;
+        iterations += 1;
+
+        if !merged_at_least_once {
+            break;
+        }
+
+        println!("[{iterations}] {:?}", ranges);
+    }
+
+    println!("Iterations: {iterations}");
+    println!("Merged ranges:\n{:?}", ranges);
+
+    0
 }
 
 #[derive(Clone, Eq, Hash, PartialEq)]
@@ -57,6 +87,17 @@ impl Sensor {
         let diff_y = self.coord.y.abs_diff(coord.y);
 
         (diff_x + diff_y) as usize <= self.radius
+    }
+
+    fn range_for_y(&self, y: &i32) -> Range<i32> {
+        let y_diff = self.coord.y.abs_diff(*y) as i32;
+
+        if y_diff > self.radius as i32 {
+            return 0..0; // Empty range
+        }
+
+        let x_diff = self.radius as i32 - y_diff;
+        (self.coord.x - x_diff)..(self.coord.x + x_diff + 1)
     }
 }
 
@@ -86,4 +127,75 @@ fn parse_line(line: &str) -> (Sensor, Coord) {
         },
         beacon,
     )
+}
+
+fn range_union<N>(r1: &Range<N>, r2: &Range<N>) -> Option<Range<N>>
+where
+    N: Copy + Ord + PartialOrd,
+{
+    if r1.start <= r2.start && r1.end > r2.start {
+        return Some(r1.start..r1.end.max(r2.end));
+    }
+
+    if r2.start <= r1.start && r2.end > r1.start {
+        return Some(r2.start..r1.end.max(r2.end));
+    }
+
+    None
+}
+
+fn merge_ranges<N>(ranges: &[Range<N>]) -> (Vec<Range<N>>, bool)
+where
+    N: Copy + Debug + Ord + PartialOrd,
+{
+    let mut merged_at_least_once = false;
+    let mut merged_ranges = vec![];
+    let mut processed_ranges = vec![];
+
+    println!("  Got {} ranges", ranges.len());
+
+    for (index, r1) in ranges.iter().enumerate() {
+        let inner_start = index + 1;
+        let mut merged = false;
+
+        if inner_start >= ranges.len() {
+            if !processed_ranges.contains(&index) {
+                println!("  [{index}] Adding the last item in the range");
+                merged_ranges.push(r1.clone());
+            }
+            break;
+        }
+
+        if processed_ranges.contains(&index) {
+            continue;
+        }
+
+        for (inner_index, r2) in ranges[inner_start..].iter().enumerate() {
+            let inner_index = inner_index + inner_start;
+
+            if processed_ranges.contains(&inner_index) {
+                continue;
+            }
+
+            println!("  Compare {:?} and {:?}", r1, r2);
+            if let Some(range) = range_union(r1, r2) {
+                println!("    [{index}], [{inner_index}] Merged to: {:?}", range);
+                merged_ranges.push(range);
+                processed_ranges.push(index);
+                processed_ranges.push(inner_index);
+                merged = true;
+                merged_at_least_once = true;
+                break;
+            }
+        }
+
+        if !merged {
+            println!("    [{index}] Wasn't nerged, so push too");
+            merged_ranges.push(r1.clone());
+        } else {
+            println!("    [{index}] Was already merged");
+        }
+    }
+
+    (merged_ranges, merged_at_least_once)
 }
